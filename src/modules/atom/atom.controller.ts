afc2408accaf66104332917c91eab596ca29609f
mudassirpk <mudassir.pk.mp@gmail.com>
2024-11-20 17:06:42 +0500

    add: get one service

--- src/modules/atom/atom.controller.ts
+++ src/modules/atom/atom.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Delete, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param } from '@nestjs/common';
 import { AtomService } from './atom.service';
 
 @Controller('atom')
 export class AtomController {
   constructor(private readonly atomService: AtomService) {}
 
-  @Delete(':/atomId')
+  @Delete(':atomId')
   delete(@Param('atomId') atomId: string) {
     return this.atomService.delete({ atomId: +atomId });
+  }
+
+  @Get(':atomId')
+  one(@Param('atomId') atomId: string) {
+    return this.atomService.one(+atomId);
   }
 }
